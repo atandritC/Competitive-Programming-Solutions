5e1d8b8c1f158e574b96212950e0640fdbc47532
Atandrit Chatterjee <atandrit04chatterjee@gmail.com>
2024-07-05 20:51:23 +0530

    Lv. 1

---
+++ TLE's CP-31 Sheet/800/B_Sequence_Game.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Define common macros
+#define int long long
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+#define pb push_back
+#define F first
+#define S second
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vpii vector<pair<int, int>>
+#define FAST_IO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define MOD 1000000007
+
+// Global variables and constants
+const int INF = 1e18;
+
+int __gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return __gcd(b, a % b);
+}
+
+// Function for solving the problem
+void t_soln()
+{
+    int n;
+    cin >> n;
+    vi a(n);
+    for (auto &x : a)
+        cin >> x;
+
+    vi res;
+    res.pb(a[0]);
+    for (int i = 1; i < n; i++)
+    {
+        if (res[res.size() - 1] <= a[i])
+            res.pb(a[i]);
+        else
+        {
+            res.pb(a[i]);
+            res.pb(a[i]);
+        }
+    }
+
+    cout << res.size() << '\n';
+    for (auto &x : res)
+        cout << x << ' ';
+    cout << '\n';
+}
+
+int32_t main()
+{
+    FAST_IO;
+
+    int t = 1; // Number of test cases
+    cin >> t;
+
+    while (t--)
+    {
+        t_soln();
+    }
+
+    return 0;
+}
