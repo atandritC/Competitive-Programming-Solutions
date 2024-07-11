1c2c72bf87e3a4e59772f835b2223311aaa1b68b
Atandrit Chatterjee <atandrit04chatterjee@gmail.com>
2024-07-11 17:12:23 +0530

    Starters 142 Division 4

---
+++ Contests/Number_Hunt.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Define common macros
+#define int long long
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+#define pb push_back
+#define F first
+#define S second
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vpii vector<pair<int, int>>
+#define FAST_IO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define MOD 1000000007
+
+// Global variables and constants
+const int INF = 1e18;
+
+int __gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return __gcd(b, a % b);
+}
+
+bool isPrime(int n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    return true;
+}
+
+// Function for solving the problem
+void t_soln()
+{
+    int n;
+    cin >> n;
+
+    if (n < 3)
+        cout << "6\n";
+    else
+    {
+        vi pr;
+        while (pr.size() < 2)
+        {
+            if (isPrime(n))
+                pr.pb(n);
+            n++;
+        }
+        cout << pr[0] * pr[1] << '\n';
+    }
+}
+
+int32_t main()
+{
+    FAST_IO;
+
+    int t = 1; // Number of test cases
+    cin >> t;
+
+    while (t--)
+    {
+        t_soln();
+    }
+
+    return 0;
+}
