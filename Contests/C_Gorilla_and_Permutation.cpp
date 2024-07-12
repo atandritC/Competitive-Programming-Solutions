22dc4bd9fefaea7a34a72d4506a87466db206950
Atandrit Chatterjee <atandrit04chatterjee@gmail.com>
2024-07-12 19:56:49 +0530

    Codeforces Round 957 (Div. 3)

---
+++ Contests/C_Gorilla_and_Permutation.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Define common macros
+#define int long long
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+#define pb push_back
+#define F first
+#define S second
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vpii vector<pair<int, int>>
+#define FAST_IO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define MOD 1000000007
+
+// Global variables and constants
+const int INF = 1e18;
+
+int __gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return __gcd(b, a % b);
+}
+
+// Function for solving the problem
+void t_soln()
+{
+    int n, m, k;
+    cin >> n >> m >> k;
+    for (int i = n; i > 0; i--)
+    {
+        if (i <= m)
+            break;
+        cout << i << ' ';
+    }
+    for (int i = 1; i <= m; i++)
+        i < m ? cout << i << ' ' : cout << i;
+    cout << '\n';
+}
+
+int32_t main()
+{
+    FAST_IO;
+
+    int t = 1; // Number of test cases
+    cin >> t;
+
+    while (t--)
+    {
+        t_soln();
+    }
+
+    return 0;
+}
