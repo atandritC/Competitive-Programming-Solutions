3bd78dc0cf144e201df82b2c0259c42014c6c974
Atandrit Chatterjee <atandrit04chatterjee@gmail.com>
2024-07-27 20:48:04 +0530

    Lv. 1

---
+++ TLE's CP-31 Sheet/800/B_Blank_Space.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Define common macros
+#define int long long
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+#define pb push_back
+#define F first
+#define S second
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vpii vector<pair<int, int>>
+#define FAST_IO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define MOD 1000000007
+
+// Global variables and constants
+const int INF = 1e18;
+
+int __gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return __gcd(b, a % b);
+}
+
+// Function for solving the problem
+void t_soln()
+{
+    int n;
+    cin >> n;
+    int bS = 0, res = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        if (!x)
+            bS++;
+        else
+        {
+            res = max(res, bS);
+            bS = 0;
+        }
+    }
+    res = max(res, bS);
+    cout << res << '\n';
+}
+
+int32_t main()
+{
+    FAST_IO;
+
+    int t = 1; // Number of test cases
+    cin >> t;
+
+    while (t--)
+    {
+        t_soln();
+    }
+
+    return 0;
+}
